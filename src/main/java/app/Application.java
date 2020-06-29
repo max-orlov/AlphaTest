658209d3c944ebbc7067c7469b4ab8dae4e7ffdd
Max <46849071+max-orlov@users.noreply.github.com>
2020-06-29 14:04:55 +0300

    Update Application.java

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -7,9 +7,8 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException, SAXException {
-
-         //Заполнить базу данных
-        XMLParser.DocParser();
+        
+        XMLParser.docParsingAndSaveToDataBase();
 
     }
 }
